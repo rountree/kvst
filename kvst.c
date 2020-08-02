7ce7702a2bbc970b2b76d80b1b53ab6ef68b8334
Barry <rountree4@llnl.gov>
2020-08-02 14:01:06 -0700

    Buggy.  See below.
    Only rank 0 is executing.  Does not return from flux_reactor_run().

--- kvst.c
+++ kvst.c
@@ -3,6 +3,7 @@
  * Instructions at the bottom of this file
  */
 #define _GNU_SOURCE 1
+#include <assert.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
@@ -12,37 +13,90 @@
 #include <jansson.h>
 #include <flux/core.h>
 
-const char default_service_name[] = "var";
+const char default_service_name[] = "fluxt";
 static uint32_t rank, size;
-
-
+static const int NO_FLAGS = 0;
+static const char const * DEFAULT_NAMESPACE = NULL;
+#define HELLO flux_log(NULL, LOG_NOTICE, "%s:%d rank %" PRIu32 " size %" PRIu32 ".\n", __FILE__, __LINE__, rank, size)
 
 int mod_main (flux_t *h, int argc, char **argv)
 {
-    flux_future_t *f;
+	int rc;
 
-    // Get rank via flux_get_rank() and print results to stderr.
-    flux_get_rank(h, &rank);
-    flux_get_size(h, &size);
-    flux_log(NULL, LOG_NOTICE, "QQQ Hello from rank %" PRIu32 " of %" PRIu32 ".\n", rank, size);
+	// Get rank via flux_get_rank() and print results to stderr.
+	flux_get_rank(h, &rank);
+	flux_get_size(h, &size);
+	flux_log(NULL, LOG_NOTICE, "%s:%d QQQ Hello from rank %" PRIu32 " of %" PRIu32 ".\n", __FILE__, __LINE__, rank, size);
+	HELLO;
     
 
-    // Create the kvs namespace.  Nevermind.  Doesn't work.
-    /*
-    f = flux_kvs_namespace_create( h, "variorum", FLUX_USERID_UNKNOWN, 0 );
-    if (!f || flux_future_get (f, NULL) < 0) { flux_log_error (h, "flux_service_register"); }
-    flux_future_destroy (f);
-    */
+	// Create the kvs namespace.  Nevermind.  Doesn't work.
+	/*
+	f = flux_kvs_namespace_create( h, "variorum", FLUX_USERID_UNKNOWN, 0 );
+	if (!f || flux_future_get (f, NULL) < 0) { flux_log_error (h, "flux_service_register"); }
+	flux_future_destroy (f);
+	*/
 
-    // Start the service.
-    flux_reactor_run (flux_get_reactor (h), 0);
+	// Start the service.
+	flux_reactor_t *kvs_reactor = flux_get_reactor(h);
+	HELLO;
+	assert( NULL != kvs_reactor );
+	HELLO;
+	rc = flux_reactor_run ( kvs_reactor, NO_FLAGS );
+	HELLO;
+	assert( -1 != rc );
+	HELLO;
 
-    // Unregister service.
-    f = flux_service_unregister (h, default_service_name);    
-    if (!f || flux_future_get (f, NULL) < 0) { flux_log_error (h, "flux_service_register"); }
-    flux_future_destroy (f);
+	// Unregister service.
+	flux_future_t *kvs_unregister_future;
+	HELLO;
+	kvs_unregister_future = flux_service_unregister (h, default_service_name);    
+	HELLO;
+	flux_log(NULL, LOG_NOTICE, "QQQ rank %" PRIu32 " of %" PRIu32 " waiting for confirmation of unregistration.\n", rank, size);
+	HELLO;
+	assert( NULL != kvs_unregister_future );
+	HELLO;
+	rc = flux_future_wait_for( kvs_unregister_future, 10.0 );
+	HELLO;
+	assert( rc != -1 );
+	HELLO;
+	flux_future_destroy ( kvs_unregister_future );
+	HELLO;
+	
 
-    return 0;
+	// Allocate the kvs transaction
+	flux_kvs_txn_t *kvs_txn = flux_kvs_txn_create();
+	HELLO;
+	assert( NULL != kvs_txn );
+	HELLO;
+
+	// Create an entry from the kvs
+	rc = flux_kvs_txn_put( kvs_txn, NO_FLAGS, "mykey", "myvalue" );
+	HELLO;
+	assert( -1 != rc );
+	HELLO;
+
+	// Commit the key+value.
+	flux_future_t *kvs_future = flux_kvs_commit( h, DEFAULT_NAMESPACE, NO_FLAGS, kvs_txn );
+	HELLO;
+	assert( NULL != kvs_future );
+	HELLO;
+
+	// Wait for confirmation
+	flux_log(NULL, LOG_NOTICE, "QQQ rank %" PRIu32 " of %" PRIu32 " waiting for confirmation of commit.\n", rank, size);
+	HELLO;
+	rc = flux_future_wait_for( kvs_future, 10.0 );
+	HELLO;
+	assert( rc != -1 );
+	HELLO;
+	flux_log(NULL, LOG_NOTICE, "QQQ rank %" PRIu32 " of %" PRIu32 " commit confirmed.\n", rank, size);
+	HELLO;
+
+	// Destroy our future.
+	flux_future_destroy( kvs_future );
+	HELLO;
+
+	return 0;
 }
 
-MOD_NAME ("var");
+MOD_NAME ("fluxt");
--- salloc.sh
+++ salloc.sh
@@ -7,5 +7,5 @@ echo `date` The flux we will be using is: `which flux`
 
 echo `date` running salloc
 #salloc --nodes=4 --sockets-per-node=2 --cores-per-socket=18 --threads-per-core=1 --time=12 --partition=pdebug 	
-salloc --nodes=4 --time=12 --partition=pdebug 	
+salloc --nodes=2 --time=12 --partition=pdebug 	
 
