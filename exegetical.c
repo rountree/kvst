717765646fc87a5749c6adf43d8e376b9700b85a
Barry <rountree4@llnl.gov>
2020-08-03 06:09:10 -0700

    Message response and timer work.

--- exegetical.c
+++ exegetical.c
@@ -21,11 +21,19 @@ static uint32_t rank, size;
 static const int NO_FLAGS = 0;
 #define HELLO flux_log(h, LOG_CRIT, "%s:%d rank %" PRIu32 " size %" PRIu32 ".\n", __FILE__, __LINE__, rank, size)
 
+void timer_handler( flux_reactor_t *r, flux_watcher_t *w, int revents, void* arg ){
+
+	flux_t *h = (flux_t*)arg;
+	flux_get_rank(h, &rank);
+	flux_get_size(h, &size);
+
+	flux_log(h, LOG_CRIT, "%s:%d rank %" PRIu32 " size %" PRIu32 ".  Timer!\n", __FILE__, __LINE__, rank, size);
+}
+
 static void foo_cb (flux_t *h, flux_msg_handler_t *mh, const flux_msg_t *msg, void *arg){
 
 	const void *data;
 	int size, rc;
-
 
 	rc = flux_msg_get_payload (msg, &data, &size);
 	if (-1 == rc){
@@ -35,8 +43,10 @@ static void foo_cb (flux_t *h, flux_msg_handler_t *mh, const flux_msg_t *msg, vo
 			flux_log_error (h, "flux_respond_error");
 		}
 	}
+	flux_log(h, LOG_CRIT, "%s:%d data=%s \n", __FILE__, __LINE__, (char*)data);
 
-	rc = flux_respond_raw (h, msg, data, size);
+	//rc = flux_respond_raw (h, msg, data, size);	// original
+	rc = flux_respond( h, msg, "{\"test_response\":24}" ); // this also works
 	if (-1 == rc){
 		flux_log_error (h, "echo_cb: flux_respond_raw");
 		rc = flux_respond_error (h, msg, errno, "flux_respond_raw failed");
@@ -62,6 +72,12 @@ int mod_main (flux_t *h, int argc, char **argv)
 	flux_msg_handler_t *mh = flux_msg_handler_create (h, match, foo_cb, NULL);
 	assert( mh );
 	flux_msg_handler_start(mh);
+
+	// Set up a timer.
+	flux_watcher_t* timer_watch_p = flux_timer_watcher_create( flux_get_reactor(h), 1.0, 1.0, timer_handler, h);
+	assert( timer_watch_p );
+	flux_watcher_start( timer_watch_p );
+
 
 	// Start the reactor.  Does not return from flux_reactor_run() until the module is unloaded.
 	flux_reactor_t *exegetical_reactor = flux_get_reactor(h);
